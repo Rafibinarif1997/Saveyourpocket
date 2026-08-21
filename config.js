09c55ecde51b07315985642986a977891b9a5101
Rafibinarif1997 <98096402+Rafibinarif1997@users.noreply.github.com>
2026-08-21 12:22:38 +0600

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.LAST404_CONFIG = {
-  SUPABASE_URL: "https://dmabnckfrvhiourqsxwz.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtYWJuY2tmcnZoaW91cnFzeHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyNzQ5NjYsImV4cCI6MjEwMjg1MDk2Nn0.a_4e1l-sVpSQB8_N6yQM1luePI--BinryDQ2vSJ5jeg",
-  DEMO_MODE: false
-};
